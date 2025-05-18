45db083f897fd956a87300220542e81adaa9342b
Nithish Roy <roynithish70@gmail.com>
2025-05-18 21:26:37 +0530

    Create agent.sv

---
+++ Arithmetic_adder/agent.sv
@@ -0,0 +1,43 @@
+class agent extends uvm_agent;
+
+    `uvm_component_utils(agent)
+
+     driver drv;
+
+    monitor mon;
+
+    sequencer seqr;
+
+    function new(string name = "agent", uvm_component parent = null);
+
+    super.new(name, parent);
+
+    endfunction 
+
+    function void build_phase(uvm_phase phase);
+
+    super.build_phase(phase);
+
+    drv = driver::type_id::create("drv",this);
+
+    mon = monitor::type_id::create("mon",this);
+
+    seqr = sequencer::type_id::create("seqr",this);
+
+    endfunction
+
+    function void connect_phase(uvm_phase phase);
+
+    super.connect_phase(phase);
+
+    drv.seq_item_port.connect(seqr.seq_item_export);
+
+    endfunction
+
+    task run_phase(uvm_phase phase);
+
+    super.run_phase(phase);
+
+    endtask
+
+endclass
