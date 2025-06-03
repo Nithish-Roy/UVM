e2b5083e191369a880f453005e84064e98d5cef3
Nithish Roy <roynithish70@gmail.com>
2025-06-03 18:48:44 +0530

    Create design.sv

---
+++ D_FF/rtl/design.sv
@@ -0,0 +1,20 @@
+module d_flip_flop(
+
+	input      clk,
+  	input      rst,
+	input  	   d  ,
+  	output reg q  ,
+  	output     q_b
+
+);
+  
+  always@(posedge clk)begin
+    if(!rst)
+      q <= 0;
+    else
+      q <= d;
+  end
+  
+  assign q_b = ~q;
+  
+endmodule
